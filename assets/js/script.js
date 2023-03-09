a75cc93e7b5779f3a582649a5ecfb9b306e48f12
tetaPehta <infoshadowriver@gmail.com>
2023-03-09 17:28:04 +0000

    Create variables for elements in document

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const restartBtn = document.getElementById('restart');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const submitBtn = document.getElementById('submit');
+const trueBtn = document.getElementById('True');
+const falseBtn = document.getElementById('False');
+const userScore = document.getElementById('user-score');
+const questionText = document.getElementById('question-text');
+
