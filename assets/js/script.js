1d36f80a9d44879c87d5a09164fb7865e76db6bf
tetaPehta <infoshadowriver@gmail.com>
2023-03-26 05:49:51 +0000

    Fix capital letters in questions and anwsers

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,71 +6,71 @@ const quizData = [
  {
     
     question: "NAME THE CONTINENT THAT TOM CREAN TRAVELED TO?", 
-    choiceA:"Australia",
-    choiceB:"North Pole",
-    choiceC:"Africa",
-    choiceD:"South Pole",
-    correct:"D"
+    choicea:"Australia",
+    choiceb:"North Pole",
+    choicec:"Africa",
+    choiced:"South Pole",
+    correct:"d"
         
  }, 
  {
     
     question: "WHAT NIS THE NAME OF THE ICONIC PUB IN ANNASCAUL, CO.KERRY?", 
-    choiceA:"Tom Crean Pub",
-    choiceB:"South Pole Inn",
-    choiceC:"The penguin Pub",
-    choiceD:"The Iceberg Inn",
-    correct:"B"
+    choicea:"Tom Crean Pub",
+    choiceb:"South Pole Inn",
+    choicec:"The penguin Pub",
+    choiced:"The Iceberg Inn",
+    correct:"b"
      
  }, 
  {
     
     question: "WHAT IS ON TOM CREANS HEAD IN THE FAMOUS PICTURE ON BOARD OF SHIP ENDURANCE?", 
-    choiceA:"Sock from his travels",
-    choiceB:"His captains hat",
-    choiceC:"Sleve of his jumper",
-    choiceD:"Cap of a crewman",
-    correct:"C"
+    choicea:"Sock from his travels",
+    choiceb:"His captains hat",
+    choicec:"Sleve of his jumper",
+    choiced:"Cap of a crewman",
+    correct:"c"
  
  }, 
  {
     
     question: "WHAT ARE THE NAMES OF 3 PUPIES TOM CREAN IS HOLDING IN THE STATUES OF HIS MEMORIAL PARK?", 
-    choiceA:"Toby, Nelson, Nell",
-    choiceB:"Tom, Sean, Bobby",
-    choiceC:"Jack, Jhon, Green",
-    choiceD:"James, Ruby, Terra",
-    correct:"A"
+    choicea:"Toby, Nelson, Nell",
+    choiceb:"Tom, Sean, Bobby",
+    choiceb:"Jack, Jhon, Green",
+    choiced:"James, Ruby, Terra",
+    correct:"a"
  
  }, 
  {
     
     question: "WHAT PART OF HIS BODY DID TOM CREAN SHOW TO THE LOCAL KIDS TO SCARE THEM AWAY?", 
-    choiceA:"His black toes",
-    choiceB:"His wobbly tooth",
-    choiceC:"His broken leg",
-    choiceD:"His crooked finger",
-    correct:"A"
+    choicea:"His black toes",
+    choiceb:"His wobbly tooth",
+    choicec:"His broken leg",
+    choiced:"His crooked finger",
+    correct:"a"
  
  }, 
  {
-    imgSrc: "../images/southpole.jpeg",
+    
     question: "TOM FOUND A FURRY ANIMAL ONBOARD THE SHIP TERRA NOVA IN 1910. WHAT IS THE TITLE OF THE BOOK?", 
-    choiceA:"Tom Crean's Dog",
-    choiceB:"Tom Crean's Rabbit",
-    choiceC:"Tom Crean's Fox",
-    choiceD:"Tom Crean's Wolf",
-    correct:"B"
+    choicea:"Tom Crean's Dog",
+    choiceb:"Tom Crean's Rabbit",
+    choiced:"Tom Crean's Fox",
+    choiced:"Tom Crean's Wolf",
+    correct:"b"
  
  }, 
  {
     
     question: "TOM CREANS FAMILY OPENED A BREWARY IN KENMARE, IRELAND. NAME THE ALE THEY MAKE?", 
-    choiceA:"Expedition Ale",
-    choiceB:"South Pole Ale",
-    choiceC:"The Frozen Ale",
-    choiceD:"Tom Crean's Ale",
-    correct:"A"
+    choicea:"Expedition Ale",
+    choiceb:"South Pole Ale",
+    choicec:"The Frozen Ale",
+    choiced:"Tom Crean's Ale",
+    correct:"a"
  
  },
    
